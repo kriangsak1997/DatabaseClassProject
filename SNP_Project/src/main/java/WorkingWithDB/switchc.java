132a7df343dcc1b8b6bb09ce506c8d84cdebe5fa
harvey <hghogar@gmail.com>
2020-03-21 20:11:53 +0700

    should work

--- SNP_Project/src/main/java/WorkingWithDB/switchc.java
+++ SNP_Project/src/main/java/WorkingWithDB/switchc.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 import static db.QueryExecution.execQuery;
 
 public class switchc {
-
+    Scanner in =new Scanner(System.in);
     public static void run(String month) throws SQLException {
         Scanner in = new Scanner(System.in);
         Queries q = new Queries();
+        String keep;
         String input1;
         String input2;
         String input3;
@@ -48,6 +49,10 @@ public class switchc {
                 input7=in.nextLine();
                 execQuery(q.insertInto(input1,input2,input3,input4,input5,input6,input7));
                 break;
+            case "6":
+                System.out.println("Enter your query");
+                keep=in.nextLine();
+                execQuery(keep);
             default:
                 System.out.println("please try again.");
                 System.out.println();
@@ -57,23 +62,19 @@ public class switchc {
     }
 
     public static void main(String[] args) throws SQLException {
-
-        Scanner in = new Scanner(System.in);
+        Scanner inp=new Scanner (System.in);
         String month;
         System.out.println(
                 "1: count plymorphism\n" +
                         "2: count disease\n" +
                         "3: count unclassified\n" +
                         "4: classification\n" +
-                        "5: insert");
+                        "5: insert\n" +
+                        "6: your own query"
+        );
         System.out.println("Enter number of function to run");
-        month = in.nextLine();
+        month = inp.nextLine();
         run(month);
-//        if (month !=1 ||month !=2||month !=3||month !=4||month !=5){
-//            System.out.println("invalid choice");
-//            System.out.println("choose again");
-//
-//        }
 
     }
 }
